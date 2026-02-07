a260e547ffe5cfb7dfeffde7051c036a75b0b287
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-08 00:39:23 +0800

    Add files via upload

--- src/main/java/com/dttyy/cropmod/ModBlocks.java
+++ src/main/java/com/dttyy/cropmod/ModBlocks.java
@@ -1,29 +1,36 @@
 package com.dttyy.cropmod;
 
-import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import com.dttyy.cropmod.block.BlockCropBase;
-import java.util.HashMap;
+
 import java.util.Map;
 
-@SubscribeEvent
-public static void registerItems(RegistryEvent.Register<Item> event) {
+@Mod.EventBusSubscriber(modid = CropMod.MODID)
+public class ModItems {
 
-    for (CropType type : CropType.values()) {
-        BlockCropBase cropBlock = ModBlocks.CROPS.get(type.getName());
+    public static final Map<String, Item> SEEDS = new java.util.HashMap<>();
+    public static final Map<String, Item> CROPS = new java.util.HashMap<>();
 
-        Item seed = new ItemCornSeed(cropBlock);
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
 
-        Item cropItem = new Item()
-            .setRegistryName("cropmod", type.getName())
-            .setTranslationKey("cropmod." + type.getName());
+        for (CropType type : CropType.values()) {
+            BlockCropBase cropBlock = ModBlocks.CROPS.get(type.getName());
 
-        SEEDS.put(type.getName(), seed);
-        CROPS.put(type.getName(), cropItem);
+            Item seed = new ItemCornSeed(cropBlock);
 
-        event.getRegistry().register(seed);
-        event.getRegistry().register(cropItem);
+            Item cropItem = new Item()
+                .setRegistryName("cropmod", type.getName())
+                .setTranslationKey("cropmod." + type.getName());
+
+            SEEDS.put(type.getName(), seed);
+            CROPS.put(type.getName(), cropItem);
+
+            event.getRegistry().register(seed);
+            event.getRegistry().register(cropItem);
+        }
     }
 }
