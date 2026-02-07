c9a408d4fbc8f0c24c9ae08ae715cb783106d046
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 10:24:19 +0800

    Create ModInfo.java

---
+++ src/main/java/com/yourname/modinspector/ModInfo.java
@@ -0,0 +1,9 @@
+package com.yourname.modinspector;
+
+public final class ModInfo {
+    public static final String MOD_ID = "mc_mod_inspector";
+    public static final String NAME = "MC Mod Inspector";
+    public static final String VERSION = "1.0.0";
+
+    private ModInfo() {}
+}
