1b9e4067fd1dccf7beb219a53078768a95692013
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-08 06:23:40 +0800

    Update CropType.java

--- src/main/java/com/dttyy/cropmod/CropType.java
+++ src/main/java/com/dttyy/cropmod/CropType.java
@@ -1,17 +1,33 @@
 package com.dttyy.cropmod;
 
 public enum CropType {
-    CORN("corn"),
-    TOMATO("tomato"),
-    CARROT("carrot");
+    CORN("corn", 4, 0.6F),
+    TOMATO("tomato", null, null),
+    CARROT("carrot", null, null);
 
     private final String name;
+    private final Integer hunger;
+    private final Float saturation;
 
-    CropType(String name) {
+    CropType(String name, Integer hunger, Float saturation) {
         this.name = name;
+        this.hunger = hunger;
+        this.saturation = saturation;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getHunger() {
+        return hunger;
+    }
+
+    public Float getSaturation() {
+        return saturation;
+    }
+
+    public boolean hasCustomFood() {
+        return hunger != null && saturation != null;
     }
 }
