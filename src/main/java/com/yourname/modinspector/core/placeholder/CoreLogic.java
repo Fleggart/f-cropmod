022415cd50853d3d93f4d7110ca98e69af065702
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 10:36:23 +0800

    Update CoreLogic.java

--- src/main/java/com/yourname/modinspector/core/placeholder/CoreLogic.java
+++ src/main/java/com/yourname/modinspector/core/placeholder/CoreLogic.java
@@ -4,6 +4,6 @@ public final class CoreLogic {
 
     public static void run() {
         System.out.println("Core logic executed!");
-        // 这里以后可以放模组扫描 / 许可证检查
+        // 将来可以放模组扫描 / 许可证检查
     }
 }
