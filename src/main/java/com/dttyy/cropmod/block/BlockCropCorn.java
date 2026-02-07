6b4e47345621a3dd3678fb92a9ed394a87d51fa0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 21:11:06 +0800

    Update BlockCropCorn.java

--- src/main/java/com/dttyy/cropmod/block/BlockCropCorn.java
+++ src/main/java/com/dttyy/cropmod/block/BlockCropCorn.java
@@ -7,8 +7,8 @@ import com.dttyy.cropmod.ModItems;
 public class BlockCropCorn extends BlockCrops {
 
     public BlockCropCorn() {
-        setUnlocalizedName("crop_corn");
         setRegistryName("cropmod", "crop_corn");
+        setTranslationKey("cropmod.crop_corn");
     }
 
     @Override
