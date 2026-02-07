03e75539b25ae8d4377c6bbfb82f73f684fd1630
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-07 20:57:49 +0800

    Delete src/main/java/gg.java

--- src/main/java/gg.java
+++
@@ -1,8 +0,0 @@
-@Mod(modid = "mycropmod", name = "My Crop Mod", version = "1.0")
-public class MyCropMod {
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        ModItems.init();
-        ModBlocks.init();
-    }
-}
