c31048f28c4e16c904ccf928b28b366152f0492b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-10 07:47:56 +0800

    Update CropFoodItem.java

--- src/main/java/com/dttyy/cropmod/item/CropFoodItem.java
+++ src/main/java/com/dttyy/cropmod/item/CropFoodItem.java
@@ -3,11 +3,7 @@ package com.dttyy.cropmod.item;
 import net.minecraft.item.ItemFood;
 
 public class CropFoodItem extends ItemFood {
-
-    // 构造方法：传入恢复饥饿值、饱和度、是否狼可吃
-    public CropFoodItem(int hunger, float saturation, boolean canWolfEat) {
-        super(hunger, saturation, canWolfEat);
-        // 你可以在这里设置 Item 属性，例如：
-        // this.setCreativeTab(CreativeTabs.FOOD); // 放到食物标签页
+    public CropFoodItem(int hunger, float saturation, boolean wolf) {
+        super(hunger, saturation, wolf);
     }
 }
