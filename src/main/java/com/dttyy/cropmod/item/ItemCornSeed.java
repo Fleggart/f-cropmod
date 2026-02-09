644f4f5652419cd20af329a99a391c77322f75c0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-10 07:23:47 +0800

    Update ItemCornSeed.java

--- src/main/java/com/dttyy/cropmod/item/ItemCornSeed.java
+++ src/main/java/com/dttyy/cropmod/item/ItemCornSeed.java
@@ -4,8 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ItemSeeds;
 import net.minecraft.init.Blocks;
 
-public class ItemCornSeed extends ItemSeeds {
-    public ItemCornSeed(Block crop) {
+public class CropSeedItem extends ItemSeeds {
+    public CropSeedItem(Block crop) {
         super(crop, Blocks.FARMLAND);
     }
 }
