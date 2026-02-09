8319ab4e570484d065d4c89eee8417a425ca78c6
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-10 07:34:10 +0800

    Create CropMaterialItem.java

---
+++ src/main/java/com/dttyy/cropmod/item/CropMaterialItem.java
@@ -0,0 +1,13 @@
+package com.dttyy.cropmod.item;
+
+import net.minecraft.item.Item;
+
+public class CropMaterialItem extends Item {
+
+    public CropMaterialItem() {
+        super();
+        // 你可以在构造器中设置一些普通 Item 属性，例如：
+        // this.setMaxStackSize(64);
+        // this.setCreativeTab(CreativeTabs.MATERIALS);
+    }
+}
