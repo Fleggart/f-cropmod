1b3bd8576783820c5a7d1453805e74f74e23aa6f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-11 00:05:41 +0800

    Update CropMod.java

--- src/main/java/com/dttyy/cropmod/CropMod.java
+++ src/main/java/com/dttyy/cropmod/CropMod.java
@@ -1,6 +1,7 @@
 package com.dttyy.cropmod;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CropMod.MODID, name = CropMod.NAME, version = CropMod.VERSION)
@@ -9,8 +10,12 @@ public class CropMod {
     public static final String NAME = "Crop Mod";
     public static final String VERSION = "1.0";
 
+    @SidedProxy(clientSide = "com.dttyy.cropmod.ClientProxy",
+                serverSide = "com.dttyy.cropmod.CommonProxy")
+    public static CommonProxy proxy;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        // 注册已由事件自动完成
+        proxy.registerModels();
     }
 }
