bbe330caa4b4c65dd7ae077ef0f305cd3ca1e12f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:57:10 +0800

    Update CropMod.java

--- src/main/java/com/dttyy/cropmod/CropMod.java
+++ src/main/java/com/dttyy/cropmod/CropMod.java
@@ -1,17 +1,11 @@
 package com.dttyy.cropmod;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(
         modid = ModConstants.MODID,
-        name = "Crop Mod",
-        version = "1.0"
+        name = ModConstants.NAME,
+        version = ModConstants.VERSION
 )
 public class CropMod {
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-    }
 }
