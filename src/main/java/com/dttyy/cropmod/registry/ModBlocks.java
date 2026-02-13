db6a314c5d3c5b04a116797451d324e9ea0f6189
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:31:53 +0800

    Update ModBlocks.java

--- src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
+++ src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
@@ -1,37 +1,47 @@
 package com.dttyy.cropmod.registry;
 
-import com.dttyy.cropmod.data.CropType; import com.dttyy.cropmod.data.StemCropType; import com.dttyy.cropmod.crop.BlockCropBase; import com.dttyy.cropmod.stem.BlockFruitBase; import com.dttyy.cropmod.stem.BlockStemBase;
+import com.dttyy.cropmod.ModConstants;
+import com.dttyy.cropmod.data.CropType;
+import com.dttyy.cropmod.data.StemCropType;
+import com.dttyy.cropmod.block.*;
 
-import net.minecraft.block.Block; import net.minecraftforge.event.RegistryEvent; import net.minecraftforge.fml.common.Mod; import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.HashMap; import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
-@Mod.EventBusSubscriber(modid = "cropmod") public class ModBlocks {
+@Mod.EventBusSubscriber(modid = ModConstants.MODID)
+public class ModBlocks {
 
-public static final Map<String, Block> CROPS = new HashMap<>();
-public static final Map<String, Block> STEMS = new HashMap<>();
-public static final Map<String, Block> FRUITS = new HashMap<>();
+    public static final Map<String, Block> CROPS = new HashMap<>();
+    public static final Map<String, Block> STEMS = new HashMap<>();
+    public static final Map<String, Block> FRUITS = new HashMap<>();
 
-@SubscribeEvent
-public static void registerBlocks(RegistryEvent.Register<Block> event) {
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
 
-    for (CropType type : CropType.values()) {
-        BlockCropBase crop = new BlockCropBase(type.getName());
-        CROPS.put(type.getName(), crop);
-        event.getRegistry().register(crop);
+        for (CropType type : CropType.values()) {
+
+            BlockCropGeneric crop = new BlockCropGeneric(null, null);
+            crop.setRegistryName(ModConstants.MODID, "crop_" + type.getName());
+            CROPS.put(type.getName(), crop);
+            event.getRegistry().register(crop);
+        }
+
+        for (StemCropType type : StemCropType.values()) {
+
+            BlockFruitGeneric fruit = new BlockFruitGeneric();
+            fruit.setRegistryName(ModConstants.MODID, type.getName() + "_fruit");
+            FRUITS.put(type.getName(), fruit);
+            event.getRegistry().register(fruit);
+
+            BlockStemGeneric stem = new BlockStemGeneric(fruit);
+            stem.setRegistryName(ModConstants.MODID, type.getName() + "_stem");
+            STEMS.put(type.getName(), stem);
+            event.getRegistry().register(stem);
+        }
     }
-
-    for (StemCropType type : StemCropType.values()) {
-
-        BlockFruitBase fruit = new BlockFruitBase(type.getName() + "_fruit");
-        BlockStemBase stem = new BlockStemBase(type.getName(), fruit);
-
-        FRUITS.put(type.getName(), fruit);
-        STEMS.put(type.getName(), stem);
-
-        event.getRegistry().register(fruit);
-        event.getRegistry().register(stem);
-    }
-}
-
 }
