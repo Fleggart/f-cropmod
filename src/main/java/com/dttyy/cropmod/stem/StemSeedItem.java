f9aa39f730619b3f013a8d87847c90046132e37d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:36:52 +0800

    Delete src/main/java/com/dttyy/cropmod/stem directory

--- src/main/java/com/dttyy/cropmod/stem/StemSeedItem.java
+++
@@ -1,5 +0,0 @@
-package com.dttyy.cropmod.stem;
-
-import net.minecraft.block.Block; import net.minecraft.init.Blocks; import net.minecraft.item.ItemSeeds;
-
-public class StemSeedItem extends ItemSeeds { public StemSeedItem(Block stem) { super(stem, Blocks.FARMLAND); } }
