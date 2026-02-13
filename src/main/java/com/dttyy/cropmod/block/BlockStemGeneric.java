1b4bbc99f423185a321427075849d7593f6b48e9
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 07:01:44 +0800

    Update BlockStemGeneric.java

--- src/main/java/com/dttyy/cropmod/block/BlockStemGeneric.java
+++ src/main/java/com/dttyy/cropmod/block/BlockStemGeneric.java
@@ -2,10 +2,22 @@ package com.dttyy.cropmod.block;
 
 import net.minecraft.block.BlockStem;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 
 public class BlockStemGeneric extends BlockStem {
 
+    private Item seed;
+
     public BlockStemGeneric(Block fruit) {
         super(fruit);
+    }
+
+    public void setSeed(Item seed) {
+        this.seed = seed;
+    }
+
+    @Override
+    protected Item getSeedItem() {
+        return seed;
     }
 }
