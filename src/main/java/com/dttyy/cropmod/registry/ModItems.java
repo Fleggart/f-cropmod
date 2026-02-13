96db657cebd98a1d13074d4e490e210c45429f39
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 07:42:10 +0800

    Update ModItems.java

--- src/main/java/com/dttyy/cropmod/registry/ModItems.java
+++ src/main/java/com/dttyy/cropmod/registry/ModItems.java
@@ -17,38 +17,35 @@ public class ModItems {
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> e) {
 
+        // Register normal crop seeds and foods
         for (CropType type : CropType.values()) {
             String name = type.getName();
 
-            // 种子
             Item seed = new CropSeedItem(ModBlocks.CROPS.get(name));
             seed.setRegistryName(ModConstants.MODID, name + "_seed");
             seed.setTranslationKey(ModConstants.MODID + "." + name + "_seed");
             e.getRegistry().register(seed);
 
-            // 食物
             Item food = new ItemFood(type.getHunger(), type.getSaturation(), false);
             food.setRegistryName(ModConstants.MODID, name + "_food");
             food.setTranslationKey(ModConstants.MODID + "." + name + "_food");
             e.getRegistry().register(food);
 
-            // 作物方块的 Item
             ItemBlock cropItem = new ItemBlock(ModBlocks.CROPS.get(name));
             cropItem.setRegistryName(ModBlocks.CROPS.get(name).getRegistryName());
             cropItem.setTranslationKey(ModConstants.MODID + ".crop_" + name);
             e.getRegistry().register(cropItem);
         }
 
+        // Register stem crop seeds and fruit item
         for (StemCropType type : StemCropType.values()) {
             String name = type.getName();
 
-            // 藤类种子
             Item stemSeed = new StemSeedItem(ModBlocks.STEMS.get(name));
             stemSeed.setRegistryName(ModConstants.MODID, name + "_seed");
             stemSeed.setTranslationKey(ModConstants.MODID + "." + name + "_seed");
             e.getRegistry().register(stemSeed);
 
-            // 果实方块的 Item
             ItemBlock fruitItem = new ItemBlock(ModBlocks.FRUITS.get(name));
             fruitItem.setRegistryName(ModBlocks.FRUITS.get(name).getRegistryName());
             fruitItem.setTranslationKey(ModConstants.MODID + "." + name + "_fruit");
