622caf36ee0dd4a708206d270f8af6956e3c28b0
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:27:50 +0800

    Update BlockFruitGeneric.java

--- src/main/java/com/dttyy/cropmod/block/BlockFruitGeneric.java
+++ src/main/java/com/dttyy/cropmod/block/BlockFruitGeneric.java
@@ -1,25 +1,11 @@
 package com.dttyy.cropmod.block;
 
-import net.minecraft.block.BlockCrops;
-import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
-public class BlockCropGeneric extends BlockCrops {
+public class BlockFruitGeneric extends Block {
 
-    private final Item seed;
-    private final Item crop;
-
-    public BlockCropGeneric(Item seed, Item crop) {
-        this.seed = seed;
-        this.crop = crop;
-    }
-
-    @Override
-    protected Item getSeed() {
-        return seed;
-    }
-
-    @Override
-    protected Item getCrop() {
-        return crop;
+    public BlockFruitGeneric() {
+        super(Material.GOURD);
     }
 }
