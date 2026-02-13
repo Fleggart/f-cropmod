9ac853f4c81a5f2800228d94c6748701b09f140a
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:29:19 +0800

    Create CropSeedItem.java

---
+++ src/main/java/com/dttyy/cropmod/item/CropSeedItem.java
@@ -0,0 +1,12 @@
+package com.dttyy.cropmod.item;
+
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+
+public class CropSeedItem extends ItemSeeds {
+
+    public CropSeedItem(Block crop) {
+        super(crop, Blocks.FARMLAND);
+    }
+}
