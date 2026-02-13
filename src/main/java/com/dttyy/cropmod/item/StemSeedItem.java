0e4ce5aede7b5d2e609bec464b9633978457263f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 07:04:55 +0800

    Update StemSeedItem.java

--- src/main/java/com/dttyy/cropmod/item/StemSeedItem.java
+++ src/main/java/com/dttyy/cropmod/item/StemSeedItem.java
@@ -1,11 +1,10 @@
 package com.dttyy.cropmod.item;
 
-import net.minecraft.item.ItemSeeds;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemSeeds;
 
 public class StemSeedItem extends ItemSeeds {
-
     public StemSeedItem(Block stem) {
         super(stem, Blocks.FARMLAND);
     }
