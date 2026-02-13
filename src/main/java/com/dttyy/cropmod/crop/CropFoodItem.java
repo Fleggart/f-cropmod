2194c7c6079bc9637e983f9033d3f3585179adf2
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-14 06:36:16 +0800

    Delete src/main/java/com/dttyy/cropmod/crop directory

--- src/main/java/com/dttyy/cropmod/crop/CropFoodItem.java
+++
@@ -1,5 +0,0 @@
-package com.dttyy.cropmod.crop;
-
-import net.minecraft.item.ItemFood;
-
-public class CropFoodItem extends ItemFood { public CropFoodItem(int hunger, float saturation) { super(hunger, saturation, false); } }
--- src/main/java/com/dttyy/cropmod/crop/CropMaterialItem.java
+++
@@ -1,5 +0,0 @@
-package com.dttyy.cropmod.crop;
-
-import net.minecraft.item.Item;
-
-public class CropMaterialItem extends Item {}
