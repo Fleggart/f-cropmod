cef5abe88c7a1955ddbf99ee297abf6324550884
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-16 05:04:53 +0800

    Delete src/main/java/com/dttyy/cropmod/ModConstants.java

--- src/main/java/com/dttyy/cropmod/ModConstants.java
+++
@@ -1,8 +0,0 @@
-package com.dttyy.cropmod;
-
-public class ModConstants {
-
-    public static final String MODID = "cropmod";
-    public static final String NAME = "Crop Mod";
-    public static final String VERSION = "1.0";
-}
