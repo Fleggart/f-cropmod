0329daa5ddf89a9bb04cd4dc9ce4e629587805ec
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-16 04:52:39 +0800

    Update CropMod.java

--- src/main/java/com/dttyy/cropmod/CropMod.java
+++ src/main/java/com/dttyy/cropmod/CropMod.java
@@ -1,11 +1,10 @@
 package com.dttyy.cropmod;
 
-import net.minecraftforge.fml.common.Mod;
+public final class ModConstants {
 
-@Mod(
-        modid = ModConstants.MODID,
-        name = ModConstants.NAME,
-        version = ModConstants.VERSION
-)
-public class CropMod {
+    private ModConstants() {}
+
+    public static final String MODID = "cropmod";
+    public static final String NAME = "Crop Mod";
+    public static final String VERSION = "1.0";
 }
