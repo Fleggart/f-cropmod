56d008138abd51a46fe585dcaa4c609ffbce852d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-16 05:09:46 +0800

    Update ModelRegistration.java

--- src/main/java/com/dttyy/cropmod/registry/ModelRegistration.java
+++ src/main/java/com/dttyy/cropmod/registry/ModelRegistration.java
@@ -1,6 +1,6 @@
 package com.dttyy.cropmod.registry;
 
-import com.dttyy.cropmod.ModConstants;
+import com.dttyy.cropmod.CropMod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -10,16 +10,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
-@Mod.EventBusSubscriber(modid = ModConstants.MODID, value = Side.CLIENT)
+@Mod.EventBusSubscriber(modid = CropMod.MODID, value = Side.CLIENT)
 public class ModelRegistration {
 
     @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
 
-        // 为 cropmod 下所有 item 注册模型
         for (Item item : ForgeRegistries.ITEMS) {
 
-            if (item.getRegistryName().getNamespace().equals(ModConstants.MODID)) {
+            if (item.getRegistryName().getNamespace().equals(CropMod.MODID)) {
 
                 ModelLoader.setCustomModelResourceLocation(
                         item, 0,
