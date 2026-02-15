e5d6950406123af806aa6e380a1c316206ced553
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-16 05:00:05 +0800

    Add files via upload

---
+++ src/main/java/com/dttyy/cropmod/ModConstants.java
@@ -0,0 +1,8 @@
+package com.dttyy.cropmod;
+
+public class ModConstants {
+
+    public static final String MODID = "cropmod";
+    public static final String NAME = "Crop Mod";
+    public static final String VERSION = "1.0";
+}
