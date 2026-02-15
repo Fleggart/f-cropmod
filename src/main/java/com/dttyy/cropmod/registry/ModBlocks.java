2701e3db4fdb83963a41bba6274daf053bab44ed
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-16 05:07:57 +0800

    Update ModBlocks.java

--- src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
+++ src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
@@ -1,6 +1,6 @@
 package com.dttyy.cropmod.registry;
 
-import com.dttyy.cropmod.ModConstants;
+import com.dttyy.cropmod.CropMod;
 import com.dttyy.cropmod.block.BlockCropGeneric;
 import com.dttyy.cropmod.block.BlockStemGeneric;
 import com.dttyy.cropmod.block.BlockFruitGeneric;
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-@Mod.EventBusSubscriber(modid = ModConstants.MODID)
+@Mod.EventBusSubscriber(modid = CropMod.MODID)
 public class ModBlocks {
 
     public static final Map<String, BlockCropGeneric> CROPS = new HashMap<>();
@@ -26,37 +26,32 @@ public class ModBlocks {
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
 
-        // 注册普通作物方块
         for (CropType type : CropType.values()) {
             String name = type.getName();
 
             BlockCropGeneric crop = new BlockCropGeneric(name);
-            crop.setRegistryName(ModConstants.MODID, "crop_" + name);
-            // translationKey 用于翻译，在 lang 文件中定义 tile.cropmod.crop_xxx.name
-            crop.setTranslationKey(ModConstants.MODID + ".crop_" + name);
+            crop.setRegistryName(CropMod.MODID, "crop_" + name);
+            crop.setTranslationKey(CropMod.MODID + ".crop_" + name);
             crop.setCreativeTab(CreativeTabs.MATERIALS);
 
             CROPS.put(name, crop);
             event.getRegistry().register(crop);
         }
 
-        // 注册藤类作物（果实 + 茎）
         for (StemCropType type : StemCropType.values()) {
             String name = type.getName();
 
-            // 果实方块
             BlockFruitGeneric fruit = new BlockFruitGeneric();
-            fruit.setRegistryName(ModConstants.MODID, name + "_fruit");
-            fruit.setTranslationKey(ModConstants.MODID + ".fruit_" + name);
+            fruit.setRegistryName(CropMod.MODID, name + "_fruit");
+            fruit.setTranslationKey(CropMod.MODID + ".fruit_" + name);
             fruit.setCreativeTab(CreativeTabs.MATERIALS);
 
             FRUITS.put(name, fruit);
             event.getRegistry().register(fruit);
 
-            // 茎方块
             BlockStemGeneric stem = new BlockStemGeneric(fruit);
-            stem.setRegistryName(ModConstants.MODID, name + "_stem");
-            stem.setTranslationKey(ModConstants.MODID + ".stem_" + name);
+            stem.setRegistryName(CropMod.MODID, name + "_stem");
+            stem.setTranslationKey(CropMod.MODID + ".stem_" + name);
             stem.setCreativeTab(CreativeTabs.MATERIALS);
 
             STEMS.put(name, stem);
