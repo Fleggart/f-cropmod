e2afbd3b93aa1f0680671cc19219be2fceecd077
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-02-20 16:38:18 +0800

    Update ModBlocks.java

--- src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
+++ src/main/java/com/dttyy/cropmod/registry/ModBlocks.java
@@ -2,10 +2,7 @@ package com.dttyy.cropmod.registry;
 
 import com.dttyy.cropmod.CropMod;
 import com.dttyy.cropmod.block.BlockCropGeneric;
-import com.dttyy.cropmod.block.BlockStemGeneric;
-import com.dttyy.cropmod.block.BlockFruitGeneric;
 import com.dttyy.cropmod.data.CropType;
-import com.dttyy.cropmod.data.StemCropType;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
@@ -20,8 +17,6 @@ import java.util.Map;
 public class ModBlocks {
 
     public static final Map<String, BlockCropGeneric> CROPS = new HashMap<>();
-    public static final Map<String, BlockStemGeneric> STEMS = new HashMap<>();
-    public static final Map<String, BlockFruitGeneric> FRUITS = new HashMap<>();
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
@@ -36,26 +31,6 @@ public class ModBlocks {
 
             CROPS.put(name, crop);
             event.getRegistry().register(crop);
-        }
-
-        for (StemCropType type : StemCropType.values()) {
-            String name = type.getName();
-
-            BlockFruitGeneric fruit = new BlockFruitGeneric();
-            fruit.setRegistryName(CropMod.MODID, name + "_fruit");
-            fruit.setTranslationKey(CropMod.MODID + ".fruit_" + name);
-            fruit.setCreativeTab(CreativeTabs.MATERIALS);
-
-            FRUITS.put(name, fruit);
-            event.getRegistry().register(fruit);
-
-            BlockStemGeneric stem = new BlockStemGeneric(fruit);
-            stem.setRegistryName(CropMod.MODID, name + "_stem");
-            stem.setTranslationKey(CropMod.MODID + ".stem_" + name);
-            stem.setCreativeTab(CreativeTabs.MATERIALS);
-
-            STEMS.put(name, stem);
-            event.getRegistry().register(stem);
         }
     }
 }
